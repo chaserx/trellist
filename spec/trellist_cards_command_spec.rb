c8136162f01f679a7e08b1a02c923174d8144158
Chase Southard <chase.southard@gmail.com>
2017-01-21 20:59:16 -0500

    not sure how to get aruba to work here

---
+++ spec/trellist_cards_command_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+RSpec.describe 'Trellist Cards Command', type: :aruba do
+  describe 'cards' do
+    xit 'returns a list of markdown formatted links' do
+    end
+  end
+end
